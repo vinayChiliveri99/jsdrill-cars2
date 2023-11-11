95f6f7b98a4bdb5be6b93f3baaaae37fc3a63fc3
vinaychiliverii <vinaychiliveri99@gmail.com>
2023-11-11 09:43:56 +0530

    add cars.js, problem1.js and testProblem1.js

---
+++ test/testProblem1.js
@@ -0,0 +1,8 @@
+// ==== Problem #1 ====
+// The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by calling a function that will return the data for that car. Then log the car's year, make, and model in the console log in the format of:
+//"Car 33 is a *car year goes here* *car make goes here* *car model goes here*"
+
+let inventory = require('../root/cars');
+let getById = require('../root/problem1');
+
+getById(33);
